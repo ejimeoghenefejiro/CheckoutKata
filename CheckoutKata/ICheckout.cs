6fb43f8f5f602f297d693964bede623db36570a2
Ejime Oghenefejiro <ejimeoghenefejiro@outlook.com>
2025-01-22 16:00:24 +0000

    Added ICheckout interface and ThePricingRule class

---
+++ CheckoutKata/ICheckout.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata
+{
+    /// <summary>
+    /// Defines the contract for a checkout library 
+    /// </summary>
+    public interface ICheckout
+    {
+        /// <summary>
+        /// Scan an item into checkout
+        /// </summary>
+       
+        void Scan(string item); // SKU of the item to scan
+
+        /// <summary>
+        /// Calculates the total price of all scanned items
+        /// </summary>
+        /// <returns> total price</returns>
+        int GetTotalPrice();
+    }
+}
---
+++ CheckoutKata/ThePricingRule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata
+{
+    public class ThePricingRule
+    {       
+        public class PricingRule
+        {
+            public string SKU { get; } //Stock Keeping Unit (SKU) identifier.
+            public int UnitPrice { get; } //Price per unit of the item.
+            public int? SpecialQuantity { get; } //For the special pricing rule quantity
+            public int? SpecialPrice { get; } // Total price for the special quantity.
+
+            /// <summary>
+            /// create a new instance of the PricingRule class.
+            /// </summary>
+           
+            public PricingRule(string sku, int unitPrice, int? specialQuantity = null, int? specialPrice = null)
+            {
+                SKU = sku;
+                UnitPrice = unitPrice;
+                SpecialQuantity = specialQuantity;
+                SpecialPrice = specialPrice;
+            }
+
+        }
+    }
+}
